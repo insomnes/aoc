3ffebb6c603cbe8bd8fa37973677ffd6bfc60f96
insomnes <denginm@gmail.com>
2024-12-12 15:49:56 +0100

    Fix grid in ceres

--- 2024/04_ceres/solution/grid.go
+++ 2024/04_ceres/solution/grid.go
@@ -63,17 +63,17 @@ func NewGridFromSlice[T any](slice []T, columns int) (*Grid[T], error) {
 }
 
 func NewGridFromSlices[T any](slices [][]T) (*Grid[T], error) {
-	width := len(slices[0])
+	columns := len(slices[0])
 	for _, row := range slices {
-		if len(row) != width {
+		if len(row) != columns {
 			return nil, fmt.Errorf("rows have different lengths")
 		}
 	}
-	height := len(slices)
-	g := NewGrid[T](width, height)
-	for y, row := range slices {
-		for x, cell := range row {
-			g.Set(x, y, cell)
+	rows := len(slices)
+	g := NewGrid[T](rows, columns)
+	for r, row := range slices {
+		for c, cell := range row {
+			g.Set(r, c, cell)
 		}
 	}
 	return g, nil
